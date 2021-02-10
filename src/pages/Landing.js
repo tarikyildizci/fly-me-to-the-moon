7082fca066c0b37024a61febbcc7f2775bdc24b5
Tarık Yusuf Yıldızcı <tarikyyildizci@gmail.com>
2021-02-10 19:57:00 +0300

    first commit

---
+++ src/pages/Landing.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Landing = () => {
+  return <div>Me land</div>;
+};
+
+export default Landing;
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Landing from './Landing';
+import Pricing from './Pricing';
+import Apply from './Apply';
+import Info from './Info';
+import Contact from './Contact';
+import Testimonials from './Testimonials';
+
+export { Landing, Pricing, Apply, Info, Contact, Testimonials };
